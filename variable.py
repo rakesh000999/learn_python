a593cbdbd0773efd91a40958d9ca5626b2e9fda6
rakesh000999 <rakeshjoshi6078@gmail.com>
2024-11-22 17:37:40 +0545

    first program and variables

---
+++ variable.py
@@ -0,0 +1,20 @@
+var = "Rakesh Joshi"
+print("My name is: " + var)
+
+x =str(1)
+print(x)
+print(type(x))
+
+y= int(2)
+print(y)
+print(type(y))
+
+z = float(3)
+print(z) 
+print(type(z))
+
+a , b , c = "apple", "ball", "cat"
+print(a)
+print(b)
+print(c)
+
